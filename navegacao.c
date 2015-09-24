8a1b9d7f4041f27763fe5b0f4a7d473294653944
Mateus Coelho <mateus_coelho@live.com>
2015-09-24 01:57:59 -0300

    Funcoes provavelmente funcionam, adicionada boas praticas de programacao

--- navegacao.c
+++ navegacao.c
@@ -5,9 +5,18 @@
 #define DESLIGA 0
 
 // potencias sempre de 0 a 100
+// para setar a potencia do motor, basta chamar setMotorSpeed uma vez!
 
+// Pode ser util
+struct Robo{
+	// Colocar dados e variaveis de estado importantes
+	int status;
+
+};
+typedef struct Robo Robo
 
 //Desloca o robo linearmente
+// Sentido = uma das constantes
 void setaMotor(int sentido, int potencia)
 {
    setMotorSpeed (motorB, potencia*sentido);
@@ -15,37 +24,34 @@ void setaMotor(int sentido, int potencia)
 }
 
 
-
-void GiraRobo(int sentido, int potencia, long degreeDado)
+// Gira o robo por degreeDado graus
+void giraRobo(int sentido, int potencia, long degreeDado)
 {
-	   resetGyro(S3);
-     delay (100);
-     while(1){
-       	 if(getGyroDegrees(S3) >= degreeDado*sentido)
-       	 {
-       	  //setaMotor(desliga,100);
-       	 	playSound(soundBlip);
-       	  break;
-       		}
-       	else
-	       setMotorSpeed (motorB,potencia*sentido);
-	       setMotorSpeed (motorA,potencia*sentido*(-1));
-	   }
+	resetGyro(S3);
+    delay (100);
+    setMotorSpeed (motorB,potencia*sentido);
+	setMotorSpeed (motorA, -potencia*sentido);
+
+    while(getGyroDegrees(S3) < degreeDado*sentido);
+    
+    playSound(soundBlip);
+    setMotorSpeed(DESLIGA, 0);
+    break;
 }
 
 //void RecolheBonecoComPa ()
 
 task main()
 {
-	while (1){
-		GiraRobo(HORARIO, 100, 90);
-		setaMotor(desliga, 100);
+	Robo labRobo, circRobo;
 
+	while (1){
+		giraRobo(HORARIO, 100, 90);
 
 		delay (2000);
-	  setaMotor(FRENTE, 100);
-	  delay (2000);
-	  setaMotor (TRAS, 100);
-	  delay (2000);
-  }
+		setaMotor(FRENTE, 100);
+		delay (2000);
+		setaMotor (TRAS, 100);
+		delay (2000);
+	}
 }
